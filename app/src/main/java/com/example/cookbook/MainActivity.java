d0db9eb4331f5e32a304a0a39d44621a1bf512bb
zin-ton <75830572+zin-ton@users.noreply.github.com>
2023-04-05 22:28:01 +0200

    Button done but idk how to search by enter

--- app/src/main/java/com/example/cookbook/MainActivity.java
+++ app/src/main/java/com/example/cookbook/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.cookbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText inputText;
+    Button search;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputText = (EditText) findViewById(R.id.SearchText);
+        search = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView2);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(inputText.getText());
+            }
+        });
     }
 }
