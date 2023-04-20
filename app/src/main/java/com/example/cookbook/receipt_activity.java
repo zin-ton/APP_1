07b8faee0438e4581092ba935d0f26ef0ccdb78e
zin-ton <75830572+zin-ton@users.noreply.github.com>
2023-04-20 10:38:03 +0200

    done logic of to_buy activity and checkboxes

---
+++ app/src/main/java/com/example/cookbook/receipt_activity.java
@@ -0,0 +1,24 @@
+package com.example.cookbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class receipt_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt);
+    }
+    public void to_main() {
+        Intent intent = new Intent(receipt_activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    public void onBackPressed() {
+        // super.onBackPressed();
+        to_main();
+    }
+}
